aaac342a03aee8928e2814593766d85034e1bf10
ganesh pansare <grpansare2002@gmail.com>
2025-07-21 14:26:57 +0530

    21july

--- ReligousECommBackend/src/main/java/com/ecomm/app/security/SecurityConfig.java
+++ ReligousECommBackend/src/main/java/com/ecomm/app/security/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
                     .requestMatchers("/api/products/**").permitAll()
                     .requestMatchers("/images/**").permitAll()
                     .requestMatchers("/api/images/**").permitAll()
+                    .requestMatchers("/api/roles/**").permitAll()
                     .anyRequest().authenticated()
             );
 
