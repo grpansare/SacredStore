be4d24bd6dd2c7f4261195948ed8209d8e8ce82f
ganesh pansare <grpansare2002@gmail.com>
2025-07-21 14:20:46 +0530

    21july

--- ReligousECommBackend/src/main/java/com/ecomm/app/controllers/RoleController.java
+++ ReligousECommBackend/src/main/java/com/ecomm/app/controllers/RoleController.java
@@ -3,7 +3,9 @@ package com.ecomm.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class RoleController {
 	
 	@Autowired
 	RoleRepository repository;
-	@RequestMapping("/{role}")
+	@PostMapping("/{role}")
 	public ResponseEntity<?> addRoles(@PathVariable String role){
 		try {
 			Role urole=new Role();
@@ -35,7 +37,7 @@ public class RoleController {
 			return new ResponseEntity(urole,HttpStatus.CREATED);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e);
 
 			return new ResponseEntity(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
